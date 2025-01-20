ae8b26de939b1955bb836b625807c0addaed45a3
santiago1708 <vela123tique@gmail.com>
2025-01-20 13:35:27 -0500

    se agregaron svg para el diseño del programa

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,8 +1,24 @@
+import { prisma } from '@/src/lib/prisma'
+import CategoryIcon from '../ui/CategoryIcon'
 
-export default function OrderSidebar() {
+async function getCatergories() {
+    return await prisma.category.findMany()
+}
+
+export default async function OrderSidebar() {
+
+    const categories = await getCatergories()
+
     return (
         <aside className="md:w-72 md:h-screen bg-white">
-            OrderSidebar
+            <nav className='mt-10'>
+                { categories.map(category => (
+                    <CategoryIcon 
+                        key={category.id}
+                        category={category}
+                    />
+                ))}
+            </nav>
         </aside>
     )
 }
---
+++ components/ui/CategoryIcon.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+import { Category } from "@prisma/client"
+
+type CategoryIconProps = {
+    category: Category
+}
+
+export default function CategoryIcon({ category }: CategoryIconProps) {
+    return (
+        <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
+            <div className="w-16 h-16 relative">
+                <Image
+                    fill
+                    src={`/icon_${category.slug}.svg`}
+                    alt="Imagen Categoria"
+                />
+            </div>
+            <p 
+                className="text-xl font-black"
+            >
+                {category.name}
+            </p>
+        </div>
+    )
+}
