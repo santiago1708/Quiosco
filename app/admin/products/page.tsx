c865874204e52b771e224719f566737fd5112eb3
santiago1708 <vela123tique@gmail.com>
2025-01-30 12:36:07 -0500

    Creando tabla con todos los productos para modificarlos o eliminarlos

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,11 +1,32 @@
+import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-export default function ProductsPage() {
+async function getProducts() {
+    const products = await prisma.product.findMany({
+        include: {
+            category: true 
+        }
+    })
+
+    return products
+}
+
+export type ProductWithCategory = Awaited<ReturnType<typeof getProducts>>
+
+export default async function ProductsPage() {
+
+    const products = await getProducts()
+
     return (
         <>
             <Heading>
                 Administrar Productos
             </Heading>
+
+            <ProductTable 
+                products={products}
+            />
         </>
     )
 }
