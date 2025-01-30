badd217989a45484815eb41f5b00e3885f9c8d32
santiago1708 <vela123tique@gmail.com>
2025-01-30 11:08:10 -0500

    se empieza a realizar la vista del administrador y a hacer reutilizables algunos componentes

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,10 @@
+import Heading from '@/components/ui/Heading'
+import React from 'react'
+
+export default function OrdersPage() {
+    return (
+        <>
+            <Heading>Administrar Ordenes</Heading>
+        </>
+    )
+}
---
+++ components/ui/Heading.tsx
@@ -0,0 +1,8 @@
+
+export default function Heading({children}: {children: React.ReactNode}) {
+    return (
+        <div className="text-2xl my-10">
+            {children}
+        </div>
+    )
+}
