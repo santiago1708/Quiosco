3070b26a88ea187120130cbf88aa96835064f39c
santiago1708 <vela123tique@gmail.com>
2025-01-31 20:21:31 -0500

    se hizo mejoras en el cache

---
+++ app/admin/orders/api/route.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@/src/lib/prisma"
+
+export async function GET() {
+
+    const orders = await prisma.order.findMany({
+        where: {
+            status: false
+        },
+        include: {
+            OrderProducts: {
+                include: {
+                    product: true
+                }
+            }
+        }
+    })
+
+    return Response.json(orders)
+}
